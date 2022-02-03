5963030fa346366d84b163152312fc7ca852aaa2
DimaTch <4dnik@mail.ru>
2022-02-03 23:12:16 +0200

    git commit -a --amend --no-edit

--- src/config/servicesConfig.ts
+++ src/config/servicesConfig.ts
@@ -5,11 +5,18 @@ import CoursesServiceFire from "../services/courses-service-firestore";
 import AuthServiceFake from "../services/auth-service-fake";
 import courseData from './courseData.json'
 import AuthServiceFire from "../services/auth-service-fire";
-
 const URL = "http://localhost:3500/courses";
+
+/**rest */
 // export const courseProvider = new CoursesServiceRest(URL); //REST
-export const courseProvider = new CoursesServiceFire("Courses", courseData.minId, courseData.maxId);
-export const college: College = new College(courseProvider);
 // export const authService = new AuthServiceJWT("http://localhost:3500");
-// export const authService = new AuthServiceFake();
+
+/**firebase */
 export const authService = new AuthServiceFire("administrators");
+export const courseProvider = new CoursesServiceFire("Courses", courseData.minId, courseData.maxId);
+
+/**fake auth */
+// export const authService = new AuthServiceFake();
+
+
+export const college: College = new College(courseProvider);
